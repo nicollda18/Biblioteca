fb7c9bf11073710c4b46a16551adcfb89cb13224
nicollda18 <nicoll.biojo@mariaelvinia.edu.co>
2019-10-09 16:47:06 -0500

    Version 1.2

--- src/biblioteca/Biblioteca.java
+++ src/biblioteca/Biblioteca.java
@@ -7,7 +7,17 @@ public class Biblioteca {
 private int numeroDeLibros;
 private int numeroDeEstantes;
 private boolean compraDeLibro;
+private String nombreDelLibro;
+private String genero;
 
+    public Biblioteca(int numeroDeLibros, int numeroDeEstantes, boolean compraDeLibro, String nombreDelLibro, String genero) {
+        this.numeroDeLibros = numeroDeLibros;
+        this.numeroDeEstantes = numeroDeEstantes;
+        this.compraDeLibro = compraDeLibro;
+        this.nombreDelLibro = nombreDelLibro;
+        this.genero = genero;
+    }
+    
     public void nuevosClientes(){
         compraDeLibro = true;
     }
@@ -20,17 +30,17 @@ private boolean compraDeLibro;
         numeroDeLibros -= 12;
     }
     
+    public void busqueda(String nomGenero, String nomLibro){
+        this.nombreDelLibro = nomLibro + " del genero " + nomGenero;
+    }
+    
+    
     public void alquilerDelibros(int nuevoNumeroDeLibros){
         if(nuevoNumeroDeLibros<=numeroDeLibros){
             System.out.println("Usted prestó 12 libros");
         }else{this.numeroDeLibros = nuevoNumeroDeLibros;
-            System.out.println("Tenemos:" + String.valueOf(nuevoNumeroDeLibros));
+            System.out.println("Tenemos:" + String.valueOf(nuevoNumeroDeLibros)+ " Libros ");
         }
     
     }
-    public static void main(String[] args) {
-        
-    }
-    
-    //sfsdfsdfsdfsjkdfloskdjf
 }
